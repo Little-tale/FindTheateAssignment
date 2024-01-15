97f0b670143185a9d5509251dfac68e7859ba2ab
judge_happily <you3192001@gmail.com>
2024-01-15 20:33:04 +0900

    아직 기능은 없는 filter액션시트 생성

--- FindTheater/MapViewController.swift
+++ FindTheater/MapViewController.swift
@@ -1,9 +1,6 @@
-//
-//  MapViewController.swift
-//  FindTheater
-//
-//  Created by Jae hyung Kim on 1/15/24.
-//
+// 1, 전체적으로 보이기 오케이
+// 2. 영화관 종류에 따른 세그먼트 또는 액션시트
+// 3.
 
 import UIKit
 import MapKit
@@ -19,6 +16,11 @@ class MapViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         var anotations: [MKPointAnnotation] = []
+
+        let rightButton = UIBarButtonItem(title: "Filter", style: .plain , target: self, action: #selector(filterAct))
+        rightButton.tintColor = .gray
+        navigationItem.rightBarButtonItem = rightButton
+        
         for theater in mapInfo{
             // anotations.append(theater.location)
             // 빈 어노테이션 생성
@@ -46,6 +48,29 @@ class MapViewController: UIViewController {
             mapView.addAnnotation(item)
         }
         
+        
+        
+    }
+    @objc func filterAct(){
+        // 잊어먹었던 액션 시트 생성법
+        // 1. UIAlertController를 이용해 액션시트를 생성한다.
+        let alertActionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        // 2. 액션을 추가한다.
+        let alerActionOfMega = UIAlertAction(title: "메가박스", style: .default)
+        let alerActionOfLotte = UIAlertAction(title: "롯데시네마", style: .default)
+        let alerActionOfCGV = UIAlertAction(title: "CGV", style: .default)
+        let alerActionOfAll = UIAlertAction(title: "전체보기", style: .default)
+        
+        alertActionSheet.addAction(alerActionOfMega)
+        alertActionSheet.addAction(alerActionOfLotte)
+        alertActionSheet.addAction(alerActionOfCGV)
+        alertActionSheet.addAction(alerActionOfAll)
+        
+        let alertActionCancel = UIAlertAction(title: "취소", style: .cancel)
+        
+        alertActionSheet.addAction(alertActionCancel)
+        present(alertActionSheet, animated: true)
     }
     
 }
+
